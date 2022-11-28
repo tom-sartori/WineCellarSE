2f3cdeafb7f6253a45d01efd4ac20d329bb0fd20
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2022-11-28 17:13:09 +0100

    Commented PartnerController.java and added a Singleton pattern

--- src/main/java/controller/partner/PartnerController.java
+++ src/main/java/controller/partner/PartnerController.java
@@ -2,18 +2,39 @@ package controller.partner;
 
 import controller.Controller;
 import entity.partner.Partner;
+import model.Model;
 import model.partner.PartnerModel;
 
-public class PartnerController extends Controller {
-    /// TODO : Class will change with the implementation of the generic controller.
+/**
+ * PartnerController class extending Controller class parametized with Partner class.
+ */
+public class PartnerController extends Controller<Partner> {
 
-    private final PartnerModel model;
+    /**
+     * instance of PartnerController to ensure Singleton design pattern
+     */
+    private static PartnerController instance;
 
-    public PartnerController() {
-        model = new PartnerModel();
+    /**
+     * private constructor for PartnerController to ensure Singleton design pattern
+     */
+    private PartnerController() { }
+
+    /**
+     * @return the instance of PartnerController to ensure Singleton design pattern
+     */
+    public static PartnerController getInstance() {
+        if(instance == null){
+            instance = new PartnerController();
+        }
+        return instance;
     }
 
-    public Partner getPartners(){
-        return model.findFirst();
+    /**
+     * @return the model of the specific Controller (PartnerModel)
+     */
+    @Override
+    protected Model<Partner> getModel() {
+        return PartnerModel.getInstance();
     }
 }
