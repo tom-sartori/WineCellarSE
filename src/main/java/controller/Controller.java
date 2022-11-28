329969059e91006113f6ac0e5acb6085d00003b0
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2022-11-28 17:15:10 +0100

    Created a Generic Controller.java

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,64 @@
 package controller;
 
-public abstract class Controller {
-	/// TODO : Generic controller.
+import entity.Entity;
+import model.ModelInterface;
+import org.bson.types.ObjectId;
+
+import java.util.Collections;
+import java.util.List;
+
+//TODO interface
+public abstract class Controller <T extends Entity<T>> {
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public T findOne(ObjectId id) {
+        T first = getModel().findOne(id);
+        first.handleOnFind();
+        return first;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<T> findAll() {
+        List<T> all = getModel().findAll();
+        all.forEach(Entity::handleOnFind);
+        Collections.sort(all);
+        return all;
+    }
+
+    /**
+     *
+     * @param entity
+     * @return
+     */
+    public ObjectId insertOne(T entity) {
+        ObjectId id = getModel().insertOne(entity);
+        entity.handleOnCreate();
+        return id;
+    }
+
+    //TODO make update function
+//    public void updateOne(ObjectId id, T t){
+//        getModel().updateOne(id,t);
+//        t.handleOnUpdate();
+//    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public long deleteOne(ObjectId id) {
+        T entityToDelete = findOne(id);
+        entityToDelete.handleOnDelete();
+        return getModel().deleteOne(id);
+    }
+
+    protected abstract ModelInterface<T> getModel();
 }
