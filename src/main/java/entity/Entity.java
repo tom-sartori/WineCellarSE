8dce502252d8c45593a86c8ebb7631a1ee4fc481
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2022-11-28 13:04:41 +0100

    added hooks to Entity.java interface

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -2,5 +2,23 @@ package entity;
 
 public interface Entity<T extends Entity<T>> extends Comparable<T> {
 
-	/// TODO : Hooks on find, create, update, delete.
+    /**
+     *  Hook method optionnaly used when the entity is found
+     */
+    default void handleOnFind() { }
+
+    /**
+     *  Hook method optionnaly used when the entity is created
+     */
+    default void handleOnCreate() { }
+
+    /**
+     *  Hook method optionnaly used when the entity is updated
+     */
+    default void handleOnUpdate() { }
+
+    /**
+     *  Hook method optionnaly used when the entity is deleted
+     */
+    default void handleOnDelete() { }
 }
