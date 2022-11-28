fd4680d52e7b84fd70f679cb877d946998eb8954
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2022-11-28 17:00:51 +0100

    implemented singleton on PartnerModel.java

--- src/main/java/model/partner/PartnerModel.java
+++ src/main/java/model/partner/PartnerModel.java
@@ -5,6 +5,18 @@ import entity.partner.Partner;
 import model.Model;
 
 public class PartnerModel extends Model<Partner> {
+
+	private static PartnerModel instance;
+
+	private PartnerModel() { }
+
+	public static PartnerModel getInstance() {
+		if (instance == null) {
+			instance = new PartnerModel();
+		}
+		return instance;
+	}
+
 	@Override
 	protected String getCollectionName() {
 		return CollectionNames.PARTNER;
