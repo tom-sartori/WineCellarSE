59b3b5aa6bcf375508179a4973842095f77f92ef
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2022-12-05 14:59:25 +0100

    Move scene size to 1280x720.

--- src/main/java/ui/app/page/application/ApplicationPage.java
+++ src/main/java/ui/app/page/application/ApplicationPage.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ui.controllers.contentarea;
+package ui.app.page.application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,16 +14,13 @@ import javafx.fxml.Initializable;
  *
  * @author Max
  */
-public class ApplicationPageController implements Initializable
-{
+public class ApplicationPage implements Initializable {
 
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
+    public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
 }
