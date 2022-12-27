cb77fcbad3794c6c6e150f712366c6aeb1d4b9bb
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2022-12-27 09:41:49 +0100

    added a selected Cellar attribute in the state

--- src/main/java/ui/app/State.java
+++ src/main/java/ui/app/State.java
@@ -1,10 +1,13 @@
 package ui.app;
 
+import persistence.entity.cellar.Cellar;
 import persistence.entity.user.User;
 
 public class State {
 	private static State instance;
 	private User currentUser;
+
+	private Cellar selectedCellar;
 
 	private State() { }
 
@@ -22,5 +25,14 @@ public class State {
 	public void setCurrentUser(User currentUser) {
 		System.out.println("The current user is now: " + currentUser.getUsername() + ". ");
 		this.currentUser = currentUser;
+	}
+
+	public Cellar getSelectedCellar() {
+		return selectedCellar;
+	}
+
+	public void setSelectedCellar(Cellar selectedCellar) {
+		System.out.println("The selected cellar is now: " + selectedCellar.getName() + ". ");
+		this.selectedCellar = selectedCellar;
 	}
 }
