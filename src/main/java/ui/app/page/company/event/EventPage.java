9657c5b33cac7bfd1caa5d8fd625c2751ee566d3
almas <alma.sorrentino@yahoo.fr>
2023-01-04 11:42:45 +0100

    creation front event

---
+++ src/main/java/ui/app/page/company/event/EventPage.java
@@ -0,0 +1,17 @@
+package ui.app.page.company.event;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EventPage implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
