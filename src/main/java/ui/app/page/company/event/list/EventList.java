3a8874dc2f07ee31cc89d3e4a8425f509056038e
almas <alma.sorrentino@yahoo.fr>
2023-01-05 17:51:36 +0100

    list events by selecting the company

--- src/main/java/ui/app/page/company/event/list/EventList.java
+++ src/main/java/ui/app/page/company/event/list/EventList.java
@@ -6,10 +6,15 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.GridPane;
+import org.bson.types.ObjectId;
+import persistence.entity.company.Company;
 import persistence.entity.event.Event;
+import ui.app.State;
 import ui.app.page.company.event.EventCard;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -18,8 +23,10 @@ public class EventList implements Initializable {
 
     @FXML
     private GridPane cardHolder;
-    private ObservableList<EventCard> cardList = FXCollections.observableArrayList();
 
+    @FXML
+    private ChoiceBox<String> select;
+    private ObservableList<EventCard> cardList = FXCollections.observableArrayList();
     private final int nbColumn = 2;
 
     /**
@@ -27,17 +34,43 @@ public class EventList implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        List<Event> eventList = Facade.getInstance().getEventList();
+        if(State.getInstance().getCurrentUser() != null){
+            List<Company> companies = Facade.getInstance().findAllCompaniesByUserId(State.getInstance().getCurrentUser().getId());
+            if(select.getItems().size() == 0){
+                for (Company c : companies){
+                    select.getItems().add(c.getName());
+                }
+            }
+
+            select.setOnAction((event) -> {
+                String selectedItem = select.getValue();
+                for(Company c : companies){
+                    if(c.getName().equals(selectedItem)){
+                        list(c.getId());
+                    }
+                }
+            });
+        } else {
+            select.hide();
+            list(null);
+        }
+    }
+
+    public void list(ObjectId company){
         cardList.clear();
+        List<Event> eventList;
+        if(company != null){
+            eventList = Facade.getInstance().getEventsByCompany(company);
+        } else {
+            eventList = Facade.getInstance().getEventList();
+        }
 
         eventList.forEach(event -> cardList.add(new EventCard(event)));
-
 
         cardHolder.setAlignment(Pos.CENTER);
         cardHolder.setVgap(30.00);
         cardHolder.setHgap(30.00);
         cardHolder.setStyle("-fx-padding:80px;-fx-alignment: center;");
-
 
         onSearch();
     }
