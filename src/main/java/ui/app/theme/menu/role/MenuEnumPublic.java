13acad2d4053cfa64ce67d0d83b90eeac21a3afd
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-01-07 00:46:49 +0100

    Trying to fix errors from merge.

--- src/main/java/ui/app/theme/menu/role/MenuEnumPublic.java
+++ src/main/java/ui/app/theme/menu/role/MenuEnumPublic.java
@@ -1,5 +1,6 @@
-package ui.app.theme.menu;
+package ui.app.theme.menu.role;
 
+import ui.app.page.company.CompanyPage;
 import ui.app.page.company.advertising.creation.AdvertisingCreation;
 import ui.app.page.company.advertising.list.AdvertisingList;
 import ui.app.page.company.event.EventPage;
