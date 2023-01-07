65f91b591e63d1cc882d3d2f4639c69f84925657
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2023-01-07 09:38:23 +0100

    Move method to create TableView in NodeCreations.java and made it static.

--- src/main/java/constant/NodeCreations.java
+++ src/main/java/constant/NodeCreations.java
@@ -1,7 +1,14 @@
 package constant;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class NodeCreations {
 
@@ -36,5 +43,32 @@ public final class NodeCreations {
         Button button = new Button(text);
         button.setCursor(javafx.scene.Cursor.HAND);
         return button;
+    }
+
+    /**
+     * Create a table view with the columns.
+     *
+     * @param headers the headers of the columns.
+     *
+     * @return the table view with the columns.
+     */
+    public static TableView<List<String>> createTable(ArrayList<String> headers){
+
+        TableView<List<String>> table = new TableView<>();
+
+        table.setFixedCellSize(25);
+        table.prefHeightProperty().bind(Bindings.size(table.getItems()).multiply(table.getFixedCellSize()).add(30));
+
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        table.setSortPolicy(param -> false);
+
+        for (String header: headers) {
+            TableColumn<List<String>, String> column = new TableColumn<>(header);
+            column.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().get(headers.indexOf(header))));
+            table.getColumns().add(column);
+        }
+
+        return table;
     }
 }
