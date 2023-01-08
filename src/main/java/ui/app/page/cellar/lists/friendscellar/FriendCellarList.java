7c6fe7a7c49b1c5486759dd6094fa89757e5fd8f
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2023-01-09 00:17:35 +0100

    added friendCellarList

---
+++ src/main/java/ui/app/page/cellar/lists/friendscellar/FriendCellarList.java
@@ -0,0 +1,39 @@
+package ui.app.page.cellar.lists.friendscellar;
+
+import facade.Facade;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.FlowPane;
+import persistence.entity.cellar.Cellar;
+import ui.app.component.card.CardComponent;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class FriendCellarList implements Initializable {
+
+    @FXML
+    private FlowPane cardHolder;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO use friends methods to implement
+
+        cardHolder.getChildren().clear();
+
+        cardHolder.setHgap(10.00);
+        cardHolder.setVgap(10.00);
+
+
+        List<Cellar> cellarList = Facade.getInstance().getPublicCellars();
+
+        for (Cellar cellar : cellarList) {
+            CardComponent card = CardComponent.createCellarCard(cellar);
+            cardHolder.getChildren().add(card);
+        }
+    }
+}
